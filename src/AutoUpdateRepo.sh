7089243bd16c3f3a81721147802f7bda6a89bf83
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-07-17 17:35:19 -0400

    updated shell script name

--- src/AutoUpdateRepo.sh
+++ src/AutoUpdateRepo.sh
@@ -5,7 +5,7 @@ git add PER_data.json
 git add PER_full_data.json
 git commit -m "updated Peru data $1"
 git push
-git add ../res/graphs
-git add ../res/tweets.dat
+git add /res/graphs
+git add /res/tweets.dat
 git commit -m "updated Peru graphs and tweet contents $1"
 git push
