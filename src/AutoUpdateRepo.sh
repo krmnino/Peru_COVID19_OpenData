a42f0c5601a83924696975cd8378be6bec6c4eb1
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-07-21 18:58:19 -0400

    updated paths for data files

--- src/AutoUpdateRepo.sh
+++ src/AutoUpdateRepo.sh
@@ -1,8 +1,8 @@
 cd ..
-git add PER_data.csv
-git add PER_full_data.csv
-git add PER_data.json
-git add PER_full_data.json
+git add ../data/PER_data.csv
+git add ../data/PER_full_data.csv
+git add ../data/PER_data.json
+git add ../data/PER_full_data.json
 git commit -m "updated Peru data $1"
 git push
 sleep 3
