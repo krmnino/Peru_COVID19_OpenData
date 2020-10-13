d80a690038013c06e25eebfe4f6e42618d27e3ac
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-10-12 22:45:46 -0400

    override emojis in tweet body

--- src/ExportUtility.py
+++ src/ExportUtility.py
@@ -83,57 +83,73 @@ def list_to_csv(parsed_data):
 def first_tweet(prev_diff, curr_diff, data, cases24h):
     out = 'ANALISIS DIARIO del #COVID19 en #PERU (1/2)\n'
     if(prev_diff[1] <= curr_diff[1]):
-        out += u'\U0001F534' + ' Casos: ' + str(int(data[1][len(data[1])-1])) + ' (+' + str(int(curr_diff[1])) + ')\n'
+        #out += u'\U0001F534' + ' Casos: ' + str(int(data[1][len(data[1])-1])) + ' (+' + str(int(curr_diff[1])) + ')\n'
+        out += 'Casos: ' + str(int(data[1][len(data[1])-1])) + ' (+' + str(int(curr_diff[1])) + ')\n'
     else:
-        out += u'\U0001F7E2' + ' Casos: ' + str(int(data[1][len(data[1])-1])) + ' (+' + str(int(curr_diff[1])) + ')\n'
+        #out += u'\U0001F7E2' + ' Casos: ' + str(int(data[1][len(data[1])-1])) + ' (+' + str(int(curr_diff[1])) + ')\n'
+        out +=  'Casos: ' + str(int(data[1][len(data[1])-1])) + ' (+' + str(int(curr_diff[1])) + ')\n'
 
-    out += ' -> (+' + str(cases24h) + ') ult. 24 hrs\n'
-    out += ' -> (+' + str(int(curr_diff[1]) - cases24h) + ') 7 dias anteriores\n'
+    out += '  -> (+' + str(cases24h) + ') ultimas 24 hrs\n'
+    out += '  -> (+' + str(int(curr_diff[1]) - cases24h) + ') 7 dias anteriores\n'
     
     if(prev_diff[6] < curr_diff[6]):
-        out += u'\U0001F534' + ' Activos: ' + str(int(curr_diff[6])) + ' ('
+        #out += u'\U0001F534' + ' Activos: ' + str(int(curr_diff[6])) + ' ('
+        out += '- Activos: ' + str(int(curr_diff[6])) + ' ('
         if(int(curr_diff[7]) > 0):
             out += '+'
         out += str(int(curr_diff[7])) + ')' + '\n'
     else:
-        out += u'\U0001F7E2' + ' Activos: ' + str(int(curr_diff[6])) + ' (' 
+        #out += u'\U0001F7E2' + ' Activos: ' + str(int(curr_diff[6])) + ' (' 
+        out += '- Activos: ' + str(int(curr_diff[6])) + ' (' 
         if(int(curr_diff[7]) > 0):
             out += '+'
         out += str(int(curr_diff[7])) + ')' + '\n'
 
     if(prev_diff[3] <= curr_diff[3]):
-        out += u'\U0001F7E2' + ' Recuperados: ' + str(int(data[4][len(data[4])-1])) + ' (+' + str(int(curr_diff[3])) + ')\n'
+        #out += u'\U0001F7E2' + ' Recuperados: ' + str(int(data[4][len(data[4])-1])) + ' (+' + str(int(curr_diff[3])) + ')\n'
+        out += '- Recuperados: ' + str(int(data[4][len(data[4])-1])) + ' (+' + str(int(curr_diff[3])) + ')\n'
     else:
-        out += u'\U0001F534' + ' Recuperados: ' + str(int(data[4][len(data[4])-1])) + ' (+' + str(int(curr_diff[3])) + ')\n'
+        #out += u'\U0001F534' + ' Recuperados: ' + str(int(data[4][len(data[4])-1])) + ' (+' + str(int(curr_diff[3])) + ')\n'
+        out += '- Recuperados: ' + str(int(data[4][len(data[4])-1])) + ' (+' + str(int(curr_diff[3])) + ')\n'
 
     if(data[20][len(data[20])-2] <= data[20][len(data[20])-1]):
-        out += u'\U0001F534' + ' Positividad diaria: ' + str((curr_diff[8]) * 100)[:5] + '%\n'
+        #out += u'\U0001F534' + ' Positividad diaria: ' + str((curr_diff[8]) * 100)[:5] + '%\n'
+        out += '- Positividad diaria: ' + str((curr_diff[8]) * 100)[:5] + '%\n'
     else:
-        out += u'\U0001F7E2' + ' Positividad diaria: ' + str((curr_diff[8]) * 100)[:5] + '%\n'
+        #out += u'\U0001F7E2' + ' Positividad diaria: ' + str((curr_diff[8]) * 100)[:5] + '%\n'
+        out += '- Positividad diaria: ' + str((curr_diff[8]) * 100)[:5] + '%\n'
 
     return out
 
 def second_tweet(prev_diff, curr_diff, data):
     out = 'ANALISIS DIARIO del #COVID19 en #PERU (2/2)\n'
     if(prev_diff[2] <= curr_diff[2]):
-        out += u'\U0001F534' + ' Fallecidos: ' + str(int(data[2][len(data[2])-1])) + ' (+' + str(int(curr_diff[2])) + ')\n'
+        #out += u'\U0001F534' + ' Fallecidos: ' + str(int(data[2][len(data[2])-1])) + ' (+' + str(int(curr_diff[2])) + ')\n'
+        out += '- Fallecidos: ' + str(int(data[2][len(data[2])-1])) + ' (+' + str(int(curr_diff[2])) + ')\n'
     else:
-        out += u'\U0001F7E2' + ' Fallecidos: ' + str(int(data[2][len(data[2])-1])) + ' (+' + str(int(curr_diff[2])) + ')\n'
+        #out += u'\U0001F7E2' + ' Fallecidos: ' + str(int(data[2][len(data[2])-1])) + ' (+' + str(int(curr_diff[2])) + ')\n'
+        out += 'Fallecidos: ' + str(int(data[2][len(data[2])-1])) + ' (+' + str(int(curr_diff[2])) + ')\n'
 
     if(data[17][len(data[17])-2] <= data[17][len(data[17])-1]):
-        out += u'\U0001F534' + ' Tasa Mortalidad: ' + str((curr_diff[9]) * 100)[:5] + '%\n'
+        #out += u'\U0001F534' + ' Tasa Mortalidad: ' + str((curr_diff[9]) * 100)[:5] + '%\n'
+        out += '- Tasa Mortalidad: ' + str((curr_diff[9]) * 100)[:5] + '%\n'
     else:
-        out += u'\U0001F7E2' + ' Tasa Mortalidad: ' + str((curr_diff[9]) * 100)[:5] + '%\n'
+        #out += u'\U0001F7E2' + ' Tasa Mortalidad: ' + str((curr_diff[9]) * 100)[:5] + '%\n'
+        out += '- Tasa Mortalidad: ' + str((curr_diff[9]) * 100)[:5] + '%\n'
 
     if(prev_diff[1] <= curr_diff[1]):
-        out += u'\U0001F534' + ' Tests: ' + str(int(data[3][len(data[2])-1])) + ' (+' + str(int(curr_diff[5])) + ')\n'
+        #out += u'\U0001F534' + ' Tests: ' + str(int(data[3][len(data[2])-1])) + ' (+' + str(int(curr_diff[5])) + ')\n'
+        out += '- Tests: ' + str(int(data[3][len(data[2])-1])) + ' (+' + str(int(curr_diff[5])) + ')\n'
     else:
-        out += u'\U0001F7E2' + ' Tests: ' + str(int(data[3][len(data[2])-1])) + ' (+' + str(int(curr_diff[5])) + ')\n'
+        #out += u'\U0001F7E2' + ' Tests: ' + str(int(data[3][len(data[2])-1])) + ' (+' + str(int(curr_diff[5])) + ')\n'
+        out += '- Tests: ' + str(int(data[3][len(data[2])-1])) + ' (+' + str(int(curr_diff[5])) + ')\n'
     
     if(data[5][len(data[5])-2] <= data[5][len(data[5])-1]):
-        out += u'\U0001F534' + ' Hospitalizados: ' + str(int(data[5][len(data[5])-1])) + ' (+' + str(int(curr_diff[4])) + ')\n'
+        #out += u'\U0001F534' + ' Hospitalizados: ' + str(int(data[5][len(data[5])-1])) + ' (+' + str(int(curr_diff[4])) + ')\n'
+        out += '- Hospitalizados: ' + str(int(data[5][len(data[5])-1])) + ' (+' + str(int(curr_diff[4])) + ')\n'
     else:
-        out += u'\U0001F7E2' + ' Hospitalizados: ' + str(int(data[5][len(data[5])-1])) + ' (' + str(int(curr_diff[4])) + ')\n'
+        #out += u'\U0001F7E2' + ' Hospitalizados: ' + str(int(data[5][len(data[5])-1])) + ' (' + str(int(curr_diff[4])) + ')\n'
+        out += '- Hospitalizados: ' + str(int(data[5][len(data[5])-1])) + ' (' + str(int(curr_diff[4])) + ')\n'
     return out
 
 def repo_tweet(date):
