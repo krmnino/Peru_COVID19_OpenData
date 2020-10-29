57ee4971b618b40ce573e773a376d1186a12e05b
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-10-29 16:55:53 -0400

    Fixed emoji indicator function, needs to be integrated in main program

--- src/TestUtility.py
+++ src/TestUtility.py
@@ -20,6 +20,47 @@ def tweet_summary(data):
         else ' (' + str(int(data[19][len(data[0])-1])) + ')\n'
     lines.append(out)
 
+    out = ''
+    out += u'\U0001F7E2 Recovered: ' + str(int(data[4][len(data[0])-1])) \
+        if data[11][len(data[0])-1] >= data[11][len(data[0])-2] \
+        else u'\U0001F534 Recovered: ' + str(int(data[18][len(data[0])-1]))
+    out += ' (+' + str(int(data[11][len(data[0])-1])) + ')\n'
+    lines.append(out)
+
+    out = ''
+    out += u'\U0001F534 Positividad Diaria: ' + str(round(data[20][len(data[0])-1] * 100, 2)) + '%' \
+        if data[20][len(data[0])-1] >= data[20][len(data[0])-2] \
+        else u'\U0001F7E2 Positividad Diaria: ' + str(round(data[20][len(data[0])-1] * 100, 2)) + '%'
+    out += ' (+' + str(round((data[20][len(data[0])-1] - data[20][len(data[0])-2]) * 100, 2)) + '%)\n' \
+        if data[20][len(data[0])-1] - data[20][len(data[0])-2] > 0 \
+        else ' (' + str(round((data[20][len(data[0])-1] - data[20][len(data[0])-2]) * 100, 2)) + '%)\n'
+    lines.append(out)
+
+    out = ''
+    out += u'\U0001F534 Fallecidos: ' + str(int(data[2][len(data[0])-1])) \
+        if data[9][len(data[0])-1] >= data[9][len(data[0])-2] \
+        else u'\U0001F7E2 Fallecidos: ' + str(int(data[2][len(data[1])-1]))
+    out += ' (+' + str(int(data[9][len(data[0])-1])) + ')\n'
+    lines.append(out)
+
+    out = ''
+    out += u'\U0001F534 Tasa Mortalidad: ' + str(round(data[17][len(data[0])-1] * 100, 4)) + '%' \
+        if data[17][len(data[0])-1] >= data[17][len(data[0])-2] \
+        else u'\U0001F7E2 Tasa Mortalidad: ' + str(round(data[17][len(data[0])-1] * 100, 4)) + '%'
+    out += ' (+' + str(round((data[17][len(data[0])-1] - data[17][len(data[0])-2]) * 100, 4)) + '%)\n' \
+        if data[17][len(data[0])-1] - data[17][len(data[0])-2] > 0 \
+        else ' (' + str(round((data[17][len(data[0])-1] - data[17][len(data[0])-2]) * 100, 4)) + '%)\n'
+    lines.append(out)
+
+    out = ''
+    out += u'\U0001F534 Hospitalizados: ' + str(int(data[5][len(data[0])-1])) \
+        if data[13][len(data[0])-2] >= data[13][len(data[0])-3] \
+        else u'\U0001F7E2 Hospitalizados: ' + str(int(data[5][len(data[0])-1]))
+    out += ' (+' + str(int(data[13][len(data[0])-1])) + ')\n' \
+        if data[13][len(data[0])-1] > 0 \
+        else ' (' + str(int(data[13][len(data[0])-1])) + ')\n'
+    lines.append(out)
+
     print(lines)
     
 
