72230978715aabab6d5a61e28fc32b5765130cd1
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-12-23 14:10:19 -0500

    added option to edit date

--- src/CommandLineUtility.py
+++ src/CommandLineUtility.py
@@ -1,20 +1,24 @@
 def print_table(data):
-    print('======================================================================')
+    print('=====================================================')
     idx = 0
     for i in data:
-        print('%2s'%(idx), '%15s'%(i + ':'), '%10s'%(data[i]))
+        print('%2s'%(idx), '%15s'%(i + ':'), '%15s'%(data[i]))
         idx += 1
-    print('======================================================================')
+    print('=====================================================')
 
 def check_data_menu(data):
     while(True):
         print_table(data)
-        user = input('Edit numbers by entering index [0-5]. Proceed? [Y/N]: ')
+        user = input('Edit numbers by entering index [0-6]. Proceed? [Y/N]: ')
         if(user == 'N' or user == 'n'):
             return 1
         elif (user == 'Y' or user == 'y'):
             return 0
-        elif(user == '0' or user == '1' or user == '2' or user == '3' or user == '4' or user == '5'):
+        elif(user == '0'):
+            data_keys = list(data.keys())
+            data[data_keys[int(user)]] = input(data_keys[int(user)] + ': ')
+            continue
+        elif(user == '1' or user == '2' or user == '3' or user == '4' or user == '5' or user == '6'):
             data_keys = list(data.keys())
             new_data = input(data_keys[int(user)] + ': ')
             if(new_data == 'N' or new_data == 'n'):
