8fd39760cac48e77cde49a8730542673df6035c8
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-12-23 13:49:22 -0500

    added command line menu to allow manual corrections on OCR readings

---
+++ src/CommandLineUtility.py
@@ -0,0 +1,33 @@
+def print_table(data):
+    print('======================================================================')
+    idx = 0
+    for i in data:
+        print('%2s'%(idx), '%15s'%(i + ':'), '%10s'%(data[i]))
+        idx += 1
+    print('======================================================================')
+
+def check_data_menu(data):
+    while(True):
+        print_table(data)
+        user = input('Edit numbers by entering index [0-5]. Proceed? [Y/N]: ')
+        if(user == 'N' or user == 'n'):
+            return 1
+        elif (user == 'Y' or user == 'y'):
+            return 0
+        elif(user == '0' or user == '1' or user == '2' or user == '3' or user == '4' or user == '5'):
+            data_keys = list(data.keys())
+            new_data = input(data_keys[int(user)] + ': ')
+            if(new_data == 'N' or new_data == 'n'):
+                print('Skip edit.')
+                continue
+            try:
+                int(new_data)
+            except:
+                print('Error: input must be an integer or [N/n] to skip.')
+                continue
+            print(data[data_keys[int(user)]])
+            data[data_keys[int(user)]] = new_data
+            continue
+        else:
+            print('Wrong input. Try Again.')
+            continue
