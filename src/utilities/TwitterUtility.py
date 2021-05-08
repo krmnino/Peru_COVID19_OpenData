71a60342ac5c7ac6c3604b86bdc4566bc94892fe
krmnino <kurt.manrique.n@gmail.com>
2021-05-08 19:40:51 -0400

    finished refactoring TwitterUtility by agregatting reply_thread() and send_thread() to TwitterAPISession

--- src/utilities/TwitterUtility.py
+++ src/utilities/TwitterUtility.py
@@ -17,11 +17,11 @@ class Tweet:
     def set_message(self, msg):
         self.message = msg
 
-    def add_image(self, msg):
+    def add_image(self, path):
         if(len(self.image_paths) >= 4):
             print("Tweet already carries 4 images with it.")
         else:
-            self.image_paths = msg
+            self.image_paths.append(path)
 
 class TwitterAPISession:
     def __init__(self, auth_config):
@@ -49,7 +49,7 @@ class TwitterAPISession:
                     break
             query_counter += 1
         if(query_counter == 15):
-            return -1
+            sys.exit('Could not retrieve tweet')
         return tweet_identificator
 
     def fetch_image(self, path, twitter_user, search_pattern, query_limit):
@@ -69,152 +69,41 @@ class TwitterAPISession:
                     break
             query_counter += 1
         if(query_counter == 15):
-            return -1
+            sys.exit('Could not retrieve tweet')
         wget.download(image_url, out=path)
         print()
 
-def tweets_generator(data, image_paths, cases24hrs):
-    # Generate tweet message with data summary
-    lines = []
+    def reply_thread(self, tweet_identificator, tweets):
+        for i in range(0, len(tweets)):
+            if(len(tweets[i].image_paths) > 0):
+                media_ids = [self.api.media_upload(tweets[i].image_paths[j]).media_id_string for j in range(0, len(tweets[i].image_paths))]
+                self.api.update_status(media_ids=media_ids, status=tweets[i].message, in_reply_to_status_id=tweet_identificator, auto_populate_reply_metadata=True)
+            else:           
+                self.api.update_status(status=tweets[i].message, in_reply_to_status_id=tweet_identificator, auto_populate_reply_metadata=True)
+            rply_tweets = self.api.user_timeline(screen_name='krm_nino', count=1, tweet_mode='extended')
+            tweet_identificator = rply_tweets[0].id
+            print('Sent tweet: ' + str(i + 1) + ' out of ' + str(len(tweets)))
+            time.sleep(2)
 
-    out = ''
-    out += u'\U0001F534 Casos: ' + str(int(data[1][len(data[0])-1])) \
-        if data[7][len(data[0])-1] >= data[7][len(data[0])-2] \
-        else u'\U0001F7E2 Casos: ' + str(int(data[1][len(data[1])-1]))
-    out += ' (+' + str(int(data[7][len(data[0])-1])) + ')\n'
-    out += ' -> (ultimos 7-dias: ' + str(int(data[7][len(data[0])-1]) - int(cases24hrs)) \
-            + ') + (ultimas 24-horas: ' + str(cases24hrs) + ')\n'
-    lines.append(out)
-
-    out = ''
-    out += u'\U0001F534 Activos: ' + str(int(data[18][len(data[0])-1])) \
-        if data[19][len(data[0])-1] >= data[19][len(data[0])-2] \
-        else u'\U0001F7E2 Activos: ' + str(int(data[18][len(data[0])-1]))
-    out += ' (+' + str(int(data[19][len(data[0])-1])) + ')\n' \
-        if data[19][len(data[0])-1] > 0 \
-        else ' (' + str(int(data[19][len(data[0])-1])) + ')\n'
-    lines.append(out)
-
-    out = ''
-    out += u'\U0001F7E2 Recuperados: ' + str(int(data[4][len(data[0])-1])) \
-        if data[11][len(data[0])-1] >= data[11][len(data[0])-2] \
-        else u'\U0001F534 Recuperados: ' + str(int(data[4][len(data[0])-1]))
-    out += ' (+' + str(int(data[11][len(data[0])-1])) + ')\n'
-    lines.append(out)
-
-    out = ''
-    out += u'\U0001F534 Hospitalizados: ' + str(int(data[5][len(data[0])-1])) \
-        if data[13][len(data[0])-1] > 0 \
-        else u'\U0001F7E2 Hospitalizados: ' + str(int(data[5][len(data[0])-1]))
-    out += ' (+' + str(int(data[13][len(data[0])-1])) + ')\n' \
-        if data[13][len(data[0])-1] > 0 \
-        else ' (' + str(int(data[13][len(data[0])-1])) + ')\n'
-    lines.append(out)
-
-    out = ''
-    out += u'\U0001F534 Fallecidos: ' + str(int(data[2][len(data[0])-1])) \
-        if data[9][len(data[0])-1] >= data[9][len(data[0])-2] \
-        else u'\U0001F7E2 Fallecidos: ' + str(int(data[2][len(data[1])-1]))
-    out += ' (+' + str(int(data[9][len(data[0])-1])) + ')\n'
-    lines.append(out)
-
-    out = ''
-    out += u'\U0001F534 Tasa Letalidad: ' + str(round(data[17][len(data[0])-1] * 100, 4)) + '%' \
-        if data[17][len(data[0])-1] >= data[17][len(data[0])-2] \
-        else u'\U0001F7E2 Tasa Letalidad: ' + str(round(data[17][len(data[0])-1] * 100, 4)) + '%'
-    out += ' (+' + str(round((data[17][len(data[0])-1] - data[17][len(data[0])-2]) * 100, 4)) + '%)\n' \
-        if data[17][len(data[0])-1] - data[17][len(data[0])-2] > 0 \
-        else ' (' + str(round((data[17][len(data[0])-1] - data[17][len(data[0])-2]) * 100, 4)) + '%)\n'
-    lines.append(out)
-
-    out = ''
-    out += u'\U0001F7E2 Pruebas (PM+PR+AG): ' + str(int(data[3][len(data[0])-1])) \
-        if data[15][len(data[0])-1] >= data[15][len(data[0])-2] \
-        else u'\U0001F534 Pruebas (PM+PR+AG): ' + str(int(data[3][len(data[1])-1]))
-    out += ' (+' + str(int(data[15][len(data[0])-1])) + ')\n'
-    lines.append(out)
-
-    out = ''
-    out += u'\U0001F534 Positividad Diaria: ' + str(round(data[20][len(data[0])-1] * 100, 2)) + '%' \
-        if data[20][len(data[0])-1] >= data[20][len(data[0])-2] \
-        else u'\U0001F7E2 Positividad Diaria: ' + str(round(data[20][len(data[0])-1] * 100, 2)) + '%'
-    out += ' (+' + str(round((data[20][len(data[0])-1] - data[20][len(data[0])-2]) * 100, 2)) + '%)\n' \
-        if data[20][len(data[0])-1] - data[20][len(data[0])-2] > 0 \
-        else ' (' + str(round((data[20][len(data[0])-1] - data[20][len(data[0])-2]) * 100, 2)) + '%)\n'
-    lines.append(out)
-
-    # Generate Tweet objects: first 2 carry data, last one links to repo
-    tweets = [Tweet(), Tweet(), Tweet()]
-    tweets[0].message = 'ANALISIS DIARIO del #COVID19 en #PERU (1/2)\n'
-    tweets[0].message += lines[0]
-    tweets[0].message += lines[1]
-    tweets[0].message += lines[2]
-    tweets[0].message += lines[3]
-    tweets[0].image_paths = image_paths[0]
-    
-    tweets[1].message = 'ANALISIS DIARIO del #COVID19 en #PERU (2/2)\n'
-    tweets[1].message += lines[4] 
-    tweets[1].message += lines[5] 
-    tweets[1].message += lines[6] 
-    tweets[1].message += lines[7]
-    tweets[1].image_paths = image_paths[1]
-
-    tweets[2].message = 'Repositorio de datos sobre el #COVID19 en #Peru actualizado al dia ' + data[0][len(data[0]) - 1] + '\n'
-    tweets[2].message += 'Sugerencias son bienvenidas!\n'
-    tweets[2].message += u'\U0001F4C8' + ' Disponible en formato .CSV y .JSON\n'
-    tweets[2].message += u'\U0001F30E' + ' WEB https://peru-covid19.com/\n'
-
-    return tweets
-
-def send_thread(auth_data, tweets):
-    tweet_identificator = ''
-    auth = tweepy.OAuthHandler(auth_data[0], auth_data[1])
-    auth.set_access_token(auth_data[2], auth_data[3])
-    try:
-        api = tweepy.API(auth)
-    except:
-        return 1
-    image_prefix_path = str(pathlib.Path().absolute()).replace('\\', '/')
-    image_prefix_path = image_prefix_path[:image_prefix_path.rfind('/')] + '/res/graphs/'
-    sent_first_tweet = False
-    for i in range(0, len(tweets)):
-        if len(tweets[i].image_paths) > 0:
-            media_ids = [api.media_upload(image_prefix_path + j).media_id_string for j in tweets[i].image_paths]
-            api.update_status(media_ids=media_ids, status=tweets[i].message, in_reply_to_status_id = tweet_identificator, auto_populate_reply_metadata=True)
-        else:
-            if(not sent_first_tweet):
-                api.update_status(status=tweets[i].message, auto_populate_reply_metadata=True)
+    def send_thread(self, tweets):
+        sent_first_tweet = False
+        tweet_identificator = None
+        for i in range(0, len(tweets)):
+            if len(tweets[i].image_paths) > 0:
+                if(not sent_first_tweet):
+                    media_ids = [self.api.media_upload(tweets[i].image_paths[j]).media_id_string for j in range(0, len(tweets[i].image_paths))]
+                    self.api.update_status(media_ids=media_ids, status=tweets[i].message, auto_populate_reply_metadata=True)
+                    sent_first_tweet = True
+                else:
+                    media_ids = [self.api.media_upload(tweets[i].image_paths[j]).media_id_string for j in range(0, len(tweets[i].image_paths))]
+                    self.api.update_status(media_ids=media_ids, status=tweets[i].message, in_reply_to_status_id=tweet_identificator, auto_populate_reply_metadata=True)
             else:
-                api.update_status(status=tweets[i].message, in_reply_to_status_id = tweet_identificator, auto_populate_reply_metadata=True)
-        rply_tweets = api.user_timeline(screen_name='krm_nino', count=1, tweet_mode='extended')
-        tweet_identificator = rply_tweets[0].id
-        print('Sent tweet: ' + str(i + 1) + ' out of 3')
-        time.sleep(2)
-    return 0
-
-def reply_thread(auth_data, tweets, tweet_identificator):
-    auth = tweepy.OAuthHandler(auth_data[0], auth_data[1])
-    auth.set_access_token(auth_data[2], auth_data[3])
-    try:
-        api = tweepy.API(auth)
-    except:
-        return 1
-    image_prefix_path = str(pathlib.Path().absolute()).replace('\\', '/')
-    image_prefix_path = image_prefix_path[:image_prefix_path.rfind('/')] + '/res/graphs/'
-    for i in range(0, len(tweets)):
-        if len(tweets[i].image_paths) > 0:
-            media_ids = [api.media_upload(image_prefix_path + j).media_id_string for j in tweets[i].image_paths]
-            api.update_status(media_ids=media_ids, status=tweets[i].message, in_reply_to_status_id = tweet_identificator, auto_populate_reply_metadata=True)
-        else:           
-            api.update_status(status=tweets[i].message, in_reply_to_status_id = tweet_identificator, auto_populate_reply_metadata=True)
-        rply_tweets = api.user_timeline(screen_name='krm_nino', count=1, tweet_mode='extended')
-        tweet_identificator = rply_tweets[0].id
-        print('Sent tweet: ' + str(i + 1) + ' out of 3')
-        time.sleep(2)
-    return 0
-
-def sleep_until(tweet_date):
-    tomorrow = datetime.datetime.strptime(tweet_date, '%Y-%m-%d') + datetime.timedelta(days=1)
-    tomorrow_str = tomorrow.strftime('%Y-%m-%d') + ' 15:00:00'
-    delta_time = datetime.datetime.strptime(tomorrow_str, '%Y-%m-%d %H:%M:%S') - datetime.datetime.now()
-    return delta_time
+                if(not sent_first_tweet):
+                    self.api.update_status(status=tweets[i].message, auto_populate_reply_metadata=True)
+                    sent_first_tweet = True
+                else:
+                    self.api.update_status(status=tweets[i].message, in_reply_to_status_id=tweet_identificator, auto_populate_reply_metadata=True)
+            rply_tweets = self.api.user_timeline(screen_name='krm_nino', count=1, tweet_mode='extended')
+            tweet_identificator = rply_tweets[0].id
+            print('Sent tweet: ' + str(i + 1) + ' out of ' + str(len(tweets)))
+            time.sleep(2)
