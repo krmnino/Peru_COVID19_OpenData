20ec1b6760f9e2cf052d47249d857b61814a2450
krmnino <kurt.manrique.n@gmail.com>
2021-07-21 01:08:45 -0400

    set current dir ad working dir

--- src/parse_pdf/ParsingTables.R
+++ src/parse_pdf/ParsingTables.R
@@ -1,6 +1,7 @@
 library("tabulizer")
 
-report_path <- "D:/temporary/reports/May_2021/coronavirus010521.pdf"
+setwd("C:/Users/kurt_/github/Peru_COVID19_Stats/src/parse_pdf")
+report_path <- "D:/temporary/DGE-MINSA_Reports/June_2021/coronavirus010621.pdf"
 output_path <- "D:/temporary/output.csv"
 
 area_pdf <- locate_areas(report_path, pages = 3)
