a0a4adb6838684b8c3d530c0eb6775b3841d44fc
krmnino <kurt.manrique.n@gmail.com>
2021-07-28 02:04:32 -0400

    set appropiate path to table

--- src/parse_pdf/RawTableProcessing/RawTableProcessing/Main.cpp
+++ src/parse_pdf/RawTableProcessing/RawTableProcessing/Main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-	std::string table_path = "../res/raw_tables/PruebasAcumuladasDepto.csv";
+	std::string table_path = "../../../../res/raw_tables/PruebasAcumuladasDepto.csv";
 	Table* casos_totales = new Table(table_path);
 	std::cout << casos_totales->get_columns() << std::endl;
 	return 0;
