eae749b0dbe620b2b2872d5e4dd1dd89ed195f35
krmnino <kurt.manrique.n@gmail.com>
2021-07-29 02:33:47 -0400

    loading raw tables using Table object

--- src/parse_pdf/RawTableProcessing/RawTableProcessing/Main.cpp
+++ src/parse_pdf/RawTableProcessing/RawTableProcessing/Main.cpp
@@ -3,8 +3,17 @@
 #include <iostream>
 
 int main() {
-	std::string table_path = "../../../../res/raw_tables/PruebasAcumuladasDepto.csv";
-	Table* casos_totales = new Table(table_path);
-	std::cout << casos_totales->get_columns() << std::endl;
+	std::string tables_abs_path = "C:/Users/kurt_/github/Peru_COVID19_Stats/res/raw_tables/";
+	std::string areas_filename = "C:/Users/kurt_/github/Peru_COVID19_Stats/src/parse_pdf/PDFAreas.csv";
+	Table* areas_table = new Table(areas_filename);
+	std::vector<Variant> table_names = areas_table->get_column_data("name");
+	
+	for (int i = 0; i < areas_table->get_rows(); i++) {
+		std::string table_full_path = tables_abs_path + *(std::string*)table_names[i].get_data() + ".csv";
+		std::cout << table_full_path << std::endl;
+		Table* raw_table = new Table(table_full_path, ';');
+		delete raw_table;
+	}
+
 	return 0;
 }
