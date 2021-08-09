b81f5098a712f09ec90fc9bb70d479f2329ba08b
krmnino <kurt.manrique.n@gmail.com>
2021-08-09 01:55:42 -0400

    updated config file to new ConfigFile version and changed report download date range

--- src/download_reports/Main.py
+++ src/download_reports/Main.py
@@ -2,8 +2,8 @@ import PDFDownload as gf
 from datetime import datetime, timedelta, date
 
 # from date to date exclusive
-start = datetime(2021, 5, 1)
-end = datetime(2021, 5, 10)
+start = datetime(2021, 7, 1)
+end = datetime(2021, 8, 1)
 
 handler = gf.PDF_Downloader(start, end)
 
