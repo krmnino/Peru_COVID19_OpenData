6d65f91ea005c667fc3e2a7c87be3b6451d4898c
krmnino <kurt.manrique.n@gmail.com>
2021-08-24 02:19:56 -0400

    moved raw table process code to its own file and created header file with function prototypes

---
+++ src/parse_pdf/RawTableProcessing/RawTableProcessing/RawTableProcessing.hpp
@@ -0,0 +1,25 @@
+#ifndef RTP
+#define RTP
+
+#include "../../../utilities/DataVariant/Variant.hpp"
+#include "../../../utilities/TableLoader/Table.hpp"
+#include "../../../utilities/ConfigLoader/ConfigLoader.hpp"
+
+#include <string>
+#include <vector>
+
+int check_data_type(std::string&);
+Variant convert_to_number(int, std::vector<std::vector<Variant>>&);
+void set_proper_col_names(Config&, Table&);
+
+int process_pa_depto(Table*&, Config*, Config*, Config*);
+int process_ca_depto(Table*&, Config*, Config*, Config*);
+int process_cp_edades(Table*&, Config*, Config*, Config*);
+int process_ma_depto(Table*&, Config*, Config*, Config*);
+int process_ca_distr_20(Table*&, Table*&, Config*, Config*, Config*);
+int process_ca_distr_21(Table*&, Table*&, Config*, Config*, Config*);
+int process_ma_distr(Table*&, Table*&, Config*, Config*, Config*);
+
+
+#endif // !RTP
+
