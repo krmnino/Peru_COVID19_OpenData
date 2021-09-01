620b736bfd3848d0ae44e2d7ba778a44651d48fe
krmnino <kurt.manrique.n@gmail.com>
2021-09-01 02:19:18 -0400

    updated date range to download pdf from Aug 30

--- src/download_reports/Main.py
+++ src/download_reports/Main.py
@@ -2,8 +2,8 @@ import PDFDownload as gf
 from datetime import datetime, timedelta, date
 
 # from date to date exclusive
-start = datetime(2021, 8, 28)
-end = datetime(2021, 8, 30)
+start = datetime(2021, 8, 30)
+end = datetime(2021, 8, 31)
 
 handler = gf.PDF_Downloader(start, end)
 
