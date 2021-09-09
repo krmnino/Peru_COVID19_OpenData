be3f2ae1975fbefdcd6db702fcb26f558cbef1b9
krmnino <kurt.manrique.n@gmail.com>
2021-09-09 02:38:28 -0400

    modified program to process and append date based on report file name

--- src/parse_pdf/CSVtoConfigLoader.py
+++ src/parse_pdf/CSVtoConfigLoader.py
@@ -5,24 +5,38 @@ sys.path.insert(0, '../utilities')
 import DataUtility as du
 import ConfigUtility as cu
 
-config = cu.Config('ParsePDFConfig.cl')
-PDFAreas = du.Table('l', filename=config.get_value('ParsePDFDir') + '/' + config.get_value('PDFAreasCSV'))
+def get_date(raw_str):
+    raw_str = raw_str[11:]
+    raw_str = raw_str[:len(raw_str)-4]
+    day = raw_str[0:2]
+    month = raw_str[2:4]
+    year = '20' + raw_str[4:]
+    return year + '-' + month + '-' + year
 
-with open(config.get_value('PDFAreasCL'), 'w') as f:
-    for i in range(0, PDFAreas.rows):
-        entry = PDFAreas.get_entry(i)
-        entry_cl = ''
-        key = ''
-        values = []
-        for i, val in enumerate(entry):
-            if(i == 0):
-                key = entry[val]
-            else:
-                values.append(entry[val])
-        entry_cl += key + '=['
-        for i in range(0, len(val)):
-            if(i != len(val)-1):
-                entry_cl += str(values[i]) + ','
-            else:
-                entry_cl += str(values[i]) + '];\n'
-        f.write(entry_cl)
+def main():
+    config = cu.Config('ParsePDFConfig.cl')
+    config_table_pages = cu.Config('PDFTablePages.cl')
+    PDFAreas = du.Table('l', filename=config.get_value('ParsePDFDir') + '/' + config.get_value('PDFAreasCSV'), delimiter=',')
+
+    with open(config.get_value('PDFAreasCL'), 'w') as f:
+        for i in range(0, PDFAreas.rows):
+            entry = PDFAreas.get_row_data(i)
+            entry_cl = ''
+            key = ''
+            values = []
+            for i, val in enumerate(entry):
+                if(i == 0):
+                    key = entry[val]
+                else:
+                    values.append(entry[val])
+            entry_cl += key + '=['
+            for i in range(0, len(val)):
+                if(i != len(val)-1):
+                    entry_cl += str(values[i]) + ','
+                else:
+                    entry_cl += str(values[i]) + '];\n'
+            f.write(entry_cl)
+        date_str = get_date(config_table_pages.get_value('ReportName'))
+        f.write('Date = ' + date_str + ';')
+    
+main()
