8b791589b6480f56e077416fe5d02702c24663c5
krmnino <kurt.manrique.n@gmail.com>
2022-01-01 05:11:33 -0500

    implemented gaussian bell plotting function

--- src/twitter_updates/StatAnalysis.py
+++ src/twitter_updates/StatAnalysis.py
@@ -1,5 +1,6 @@
 import sys
-from math import sqrt
+from math import e, pi, sqrt
+import random
 import numpy as np
 
 sys.path.insert(0, '../utilities')
@@ -18,11 +19,78 @@ class Stats:
             acc += (self.data[i] - self.mean) ** 2
         acc = acc / self.size
         self.stdev = sqrt(acc)
-        print(self.mean, self.stdev)
 
-    def plot_gauss_bell():
-        self.plot = LayeredScatterPlot()
-        return 0
+    def plot_gauss_bell(self, ofile):
+        xbell = np.array([])
+        xdata = np.array([])
+        ybell = np.array([])
+        ydata = np.array([])
+    
+        low_range = self.mean - (self.stdev * 4)
+        high_range = self.mean + (self.stdev * 4)
+        increments = (high_range - low_range) / 1000
+        count = low_range
+        while (count < high_range):
+            val = (1.0 / (self.stdev * sqrt(2 * pi))) * e ** (-0.5 * ((count - self.mean) / self.stdev) ** 2)
+            xbell = np.append(xbell, count)
+            ybell = np.append(ybell, val)
+            count += increments
+        
+        max_y_curve = np.max(ybell)
 
-data = np.array([5,98,4,94,6,46,4,6,48,2,1,854,62,6,5,4,3,65,6,6,9,869,2,1,68,3,61,9,3,57,3,3,5,553,53,3,35,9])
+        for i in range(0, len(self.data)):
+            xdata = np.append(xdata, self.data[i])
+            max_y = (1.0 / (self.stdev * sqrt(2 * pi))) * e ** (-0.5 * ((self.data[i] - self.mean) / self.stdev) ** 2)
+            ydata = np.append(ydata, random.uniform(0, max_y))
+
+        xmean = np.array([self.mean, self.mean])
+        ymean = np.array([0, max_y_curve])
+        xsd1 = np.array([self.mean - (self.stdev * 3), self.mean - (self.stdev * 3)])
+        ysd1 = np.array([0, max_y_curve])
+        xsd2 = np.array([self.mean - (self.stdev * 2), self.mean - (self.stdev * 2)])
+        ysd2 = np.array([0, max_y_curve])
+        xsd3 = np.array([self.mean - (self.stdev), self.mean - (self.stdev)])
+        ysd3 = np.array([0, max_y_curve])
+        xsd4 = np.array([self.mean + (self.stdev), self.mean + (self.stdev)])
+        ysd4 = np.array([0, max_y_curve])
+        xsd5 = np.array([self.mean + (self.stdev * 2), self.mean + (self.stdev * 2)])
+        ysd5 = np.array([0, max_y_curve])
+        xsd6 = np.array([self.mean + (self.stdev * 3), self.mean + (self.stdev * 3)])
+        ysd6 = np.array([0, max_y_curve])
+
+        n_datasets = 9
+        colors_ds = ['b', 'r', 'k', 'k', 'k', 'k', 'k', 'k', 'k']
+        linestyle_ds = ['-', '', '-', '--', '--', '--', '--', '--', '--']
+        markers_ds = ['', 'o', '', '', '', '', '', '', '']
+        enable_rolling_avg_ds = [False, False, False, False, False, False, False, False, False]
+        x_label = ''
+        y_label = ''
+        stitle = 'Stats Analysis Gaussian Bell'
+        out_file = ofile
+        ravg_days_ds = [None, None, None, None, None, None, None, None, None]
+        ravg_labels_ds = [False, False, False, False, False, False, False, False, False]
+        ravg_ydata_ds = [None, None, None, None, None, None, None, None, None]
+
+        self.plot = pu.LayeredScatterPlot(
+            n_datasets,
+            colors_ds,
+            linestyle_ds,
+            markers_ds,
+            enable_rolling_avg_ds,
+            x_label,
+            y_label,
+            [xbell, xdata, xmean, xsd1, xsd2, xsd3, xsd4, xsd5, xsd6],
+            [ybell, ydata, ymean, ysd1, ysd2, ysd3, ysd4, ysd5, ysd6],
+            stitle,
+            out_file,
+            ravg_days_ds,
+            ravg_labels_ds,
+            ravg_ydata_ds
+        )
+        self.plot.export()
+
+data = np.array([])
+for i in range(0, 100):
+    data = np.append(data, random.uniform(-100.0, 50.0))
 tmp = Stats(data)
+tmp.plot_gauss_bell('test.png')
