760750098ba418ab60d68e51c834de45a25d6eef
krmnino <kurt.manrique.n@gmail.com>
2022-01-09 18:36:27 -0500

    put together script to update remote datasets

---
+++ src/parse_pdf/UpdateDatasets.sh
@@ -0,0 +1,13 @@
+git add ../../data/CasosAcumuDistrito2020
+git add ../../data/CasosAcumuDistrito2021
+git add ../../data/CasosAcumuladosDepto
+git add ../../data/CasosPositivosEdades
+git add ../../data/MuertesAcumuladasDepto
+git add ../../data/MuertesAcumuladasDepto_SINADEF-MINSA
+git add ../../data/MuertesAcumulaDistrito
+git add ../../data/PruebasAcumuladasDepto
+git add PDFAreas.cl
+git add PDFTablePages.cl
+git add PDFAreas.csv
+git commit -m "updated Peru data $1"
+git push
