4a1b9a2cde1361f0e4e617903e14a43eee25e2ed
krmnino <kurt.manrique.n@gmail.com>
2022-01-12 20:24:52 -0500

    implemented base line program that extracts text from pdf file

---
+++ src/parse_pdf/ExtractPDFText.py
@@ -0,0 +1,19 @@
+import sys
+import PyPDF2 
+
+sys.path.insert(0, '../utilities')
+
+import ConfigUtility as cu    
+
+def main():
+    config = cu.Config('PDFTablePages.cl')
+    pdf_path = config.get_value('ReportPath') + '/' + config.get_value('ReportName')
+
+    with open(pdf_path, 'rb') as file:
+        pdf_file = PyPDF2.PdfFileReader(file) 
+        pageObj = pdf_file.getPage(int(config.get_value('DatosResumen'))-1) 
+        split_lines = pageObj.extractText().split('\n')
+        print(split_lines)
+
+main()
+    
