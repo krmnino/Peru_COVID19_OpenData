019ad87f458adeee8afc51908b7bf5dbf0061f9d
krmnino <kurt.manrique.n@gmail.com>
2022-07-12 02:54:04 -0400

    updated ParsingTables R scripts and updated config files

--- src/parse_pdf/ParsingTables_v1.R
+++ src/parse_pdf/ParsingTables_v1.R
@@ -1,95 +1,394 @@
+# Set up working environment
+working_dir <- "C:/Users/kurt_/github/Peru_COVID19_Stats/src/parse_pdf"
+setwd(working_dir)
+
+# Include dependencies
 library("tabulizer")
+source("../utilities/ConfigLoader/ConfigLoader.R")
 
-report_path <- ""
-pdf_areas_path <- "C:/Users/kurt_/github/Peru_COVID19_Stats/src/parse_pdf/config/PDFAreas.csv" 
-PDF_pages_num_dict <- "C:/Users/kurt_/github/Peru_COVID19_Stats/src/parse_pdf/config/PDFTablePages.cl"
-output_abs_path <- "C:/Users/kurt_/github/Peru_COVID19_Stats/res/raw_tables/"
-
-################################################################################
-
-PDF_table_num_pages <- file(PDF_pages_num_dict, "r")
-pg_c <- 1
-ln_c <- 1
-
-while(ln_c <= n_pages){
-  line = readLines(PDF_table_num_pages, n=1)
-  print(line)
-  if(length(line) == 0){
-    break
-  }
-  line_split = strsplit(line, '=')
-  if(pg_c > 10){
-    report_path <- paste(report_path, substr(line_split[[1]][2], 1, nchar(line_split[[1]][2])-1), sep="")
-  }
-  pg_c <- pg_c + 1
-  ln_c <- ln_c + 1
-}
-
-close(PDF_table_num_pages)
+# Set up environment for ConfigLoader
+ConfigLoader_set_env(working_dir)
 
 ################################################################################
 
-# Load csv file with PDF areas to be scanned
-pdf_areas <- read.csv(pdf_areas_path)
+# Load configuration files
+Main_Config <- ConfigLoader_init("config/ParsePDFConfig.cl")
+Areas_Config <- ConfigLoader_init("config/AreasPDF.cl")
+
+# Extract and concatenate PDF report absolute path
+report_path <- ConfigLoader_get_value(Areas_Config, "ReportPath")
+report_name <- ConfigLoader_get_value(Areas_Config, "ReportFilename")
+report_abs_path <- paste(report_path, report_name, sep="")
+
+# Extract repository top level path
+top_level_path <- ConfigLoader_get_value(Main_Config, "WindowsTopLevel")
+
+################################################################################
 
 # Pruebas acumuladas por departamento
-data <- extract_tables(report_path, area=list(pdf_areas[1,2:5]), guess=FALSE, pdf_areas[1,6])
-header <- list("Depto", "PCR", "PR", "AG", "Total")
-out_file_path <- paste(output_abs_path, pdf_areas[1,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "PADepto_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_PADepto")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "PADepto_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "PADepto_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Casos acumulados por departamento
-data <- extract_tables(report_path, area=list(pdf_areas[2,2:5]), guess=FALSE, pdf_areas[2,6])
-header <- list("Depto", "PCR", "PR", "AG", "Total")
-out_file_path <- paste(output_abs_path, pdf_areas[2,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "CADepto_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_CADepto")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "CADepto_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "CADepto_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Casos acumulados por edades
-data <- extract_tables(report_path, area=list(pdf_areas[3,2:5]), guess=FALSE, pdf_areas[3,6])
-header <- list("Grupo", "Total", "Tasa_Ataque", "Razon_Tasas")
-out_file_path <- paste(output_abs_path, pdf_areas[3,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
 
-# Muertes acumuladas por departamento
-data <- extract_tables(report_path, area=list(pdf_areas[4,2:5]), guess=FALSE, pdf_areas[4,6])
-header <- list("Depto", "Muertes_Conf", "Muertes_Sosp", "Total_Muertes_SisVig", "Muertes_SINADEF")
-out_file_path <- paste(output_abs_path, pdf_areas[4,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "CPEdades_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_CPEdades")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "CPEdades_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "CPEdades_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
+
+# Muertes acumuladas por departamento (SINADEF + MINSA)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "MADepto_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_MADepto")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "MADeptoSM_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "MADeptoSM_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Casos acumulados por distrito 2020 pt.1
-data <- extract_tables(report_path, area=list(pdf_areas[5,2:5]), guess=FALSE, pdf_areas[5,6])
-header <- list("Distrito", "Casos_Totales", "Porcentaje", "Tasa_Ataque")
-out_file_path <- paste(output_abs_path, pdf_areas[5,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "CADistr20P1_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_CADistr20P1")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "CADistr20P1_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "CADistr20P1_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Casos acumulados por distrito 2020 pt.2
-data <- extract_tables(report_path, area=list(pdf_areas[6,2:5]), guess=FALSE, pdf_areas[6,6])
-header <- list("Distrito", "Casos_Totales", "Porcentaje", "Tasa_Ataque")
-out_file_path <- paste(output_abs_path, pdf_areas[6,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "CADistr20P2_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_CADistr20P2")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "CADistr20P2_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "CADistr20P2_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Casos acumulados por distrito 2021 pt.1
-data <- extract_tables(report_path, area=list(pdf_areas[7,2:5]), guess=FALSE, pdf_areas[7,6])
-header <- list("Distrito", "Casos_Totales", "Porcentaje", "Tasa_Ataque")
-out_file_path <- paste(output_abs_path, pdf_areas[7,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "CADistr21P1_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_CADistr21P1")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "CADistr21P1_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "CADistr21P1_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Casos acumulados por distrito 2021 pt.2
-data <- extract_tables(report_path, area=list(pdf_areas[8,2:5]), guess=FALSE, pdf_areas[8,6])
-header <- list("Distrito", "Casos_Totales", "Porcentaje", "Tasa_Ataque")
-out_file_path <- paste(output_abs_path, pdf_areas[8,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "CADistr21P2_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_CADistr21P2")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "CADistr21P2_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "CADistr21P2_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Muertes acumuladas por distrito pt.1
-data <- extract_tables(report_path, area=list(pdf_areas[9,2:5]), guess=FALSE, pdf_areas[9,6])
-header <- list("Distrito", "Defunciones", "Tasa_Mortalidad")
-out_file_path <- paste(output_abs_path, pdf_areas[9,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
+
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "MADistrP1_PDFPage")
+
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_MADistrP1")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "MADistrP1_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "MADistrP1_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
+
+################################################################################
 
 # Muertes acumuladas por distrito pt.2
-data <- extract_tables(report_path, area=list(pdf_areas[10,2:5]), guess=FALSE, pdf_areas[10,6])
-header <- list("Distrito", "Defunciones", "Tasa_Mortalidad")
-out_file_path <- paste(output_abs_path, pdf_areas[10,7], sep="")
-write.table(data, out_file_path, sep=";", row.names=FALSE, col.names=header, quote=FALSE)
 
+# Extract PDF page
+page <- ConfigLoader_get_value(Main_Config, "MADistrP2_PDFPage")
 
+# Extract and covert PDF area
+area_list <- ConfigLoader_get_value(Areas_Config, "Areas_MADistrP2")
+to_double <- c()
+for(i in 1:length(area_list)){
+  to_double <- append(to_double, as.double(area_list[i]))
+}
+
+# Extract PDF table
+data <- extract_tables(report_abs_path,
+                       area=list(to_double),
+                       guess=FALSE, pages=page)
+
+# Extract table header and convert to list
+header <- ConfigLoader_get_value(Main_Config, "MADistrP2_RTHdr")
+header_list <- list()
+for(i in 1:length(header)){
+  header_list <- append(header_list, header[i])
+}
+
+# Build raw table path
+raw_table_path <- ConfigLoader_get_value(Main_Config, "MADistrP2_RT")
+raw_table_path <- paste(top_level_path, raw_table_path, sep="")
+
+# Write data to table 
+write.table(data,
+            raw_table_path, sep=";",
+            row.names=FALSE,
+            col.names=header_list,
+            quote=FALSE)
