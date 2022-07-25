3dee35160cc32e84c9dd26ac3c5f125b3493bf08
krmnino <kurt.manrique.n@gmail.com>
2022-07-24 22:54:52 -0400

    enable RawTableProcessing function calls

--- src/parse_pdf/Main.cpp
+++ src/parse_pdf/Main.cpp
@@ -37,44 +37,42 @@ int main() {
 	// Load Age Groups Index configuration file
 	config_path = main_config->get_value("AgeGroupsIndex")->get_data<std::string>();
 	cl::Config* age_index = new cl::Config(top_level_path + config_path);
-
-	//int n_tables = areas_config->get_n_entries();
 	
 	// Process PruebasAcumuladasDepto.csv
-	//{
-	//	tl::Table* input_raw_table;
-	//	process_pa_depto(input_raw_table, main_config, dept_index);
-	//	append_end_pa_depto(input_raw_table, main_config, report_date, dept_index);
-	//	//append_begin_pa_depto(input_raw_table, main_config, report_date, dept_index);
-	//	delete input_raw_table;
-	//}
+	{
+		tl::Table* input_raw_table;
+		process_pa_depto(input_raw_table, main_config, dept_index);
+		append_end_pa_depto(input_raw_table, main_config, report_date, dept_index);
+		//append_begin_pa_depto(input_raw_table, main_config, report_date, dept_index);
+		delete input_raw_table;
+	}
 
 	// Process CasosAcumuladosDepto.csv
-	//{
-	//	tl::Table* input_raw_table;
-	//	process_ca_depto(input_raw_table, main_config, dept_index);
-	//	append_end_ca_depto(input_raw_table, main_config, report_date, dept_index);
-	//	//append_begin_ca_depto(input_raw_table, main_config, report_date, dept_index);
-	//	delete input_raw_table;
-	//}
+	{
+		tl::Table* input_raw_table;
+		process_ca_depto(input_raw_table, main_config, dept_index);
+		append_end_ca_depto(input_raw_table, main_config, report_date, dept_index);
+		//append_begin_ca_depto(input_raw_table, main_config, report_date, dept_index);
+		delete input_raw_table;
+	}
 	
 	// Process CasosPositivosEdades.csv
-	//{
-	//	tl::Table* input_raw_table;
-	//	process_cp_edades(input_raw_table, main_config, age_index);
-	//	append_end_cp_edades(input_raw_table, main_config, report_date, age_index);
-	//	//append_begin_cp_edades(input_raw_table, main_config, areas_config, age_index);
-	//	delete input_raw_table;
-	//}
+	{
+		tl::Table* input_raw_table;
+		process_cp_edades(input_raw_table, main_config, age_index);
+		append_end_cp_edades(input_raw_table, main_config, report_date, age_index);
+		//append_begin_cp_edades(input_raw_table, main_config, areas_config, age_index);
+		delete input_raw_table;
+	}
 	
 	// Process MuertesAcumuladasDepto.csv
-	//{
-	//	tl::Table* input_raw_table;
-	//	process_ma_depto(input_raw_table, main_config, dept_index);
-	//	append_end_ma_depto(input_raw_table, main_config, report_date, dept_index);
-	//	//append_begin_ma_depto(input_raw_table, main_config, report_date, dept_index);
-	//	delete input_raw_table;
-	//}
+	{
+		tl::Table* input_raw_table;
+		process_ma_depto(input_raw_table, main_config, dept_index);
+		append_end_ma_depto(input_raw_table, main_config, report_date, dept_index);
+		//append_begin_ma_depto(input_raw_table, main_config, report_date, dept_index);
+		delete input_raw_table;
+	}
 
 	//{
 	//	Table* input_raw_table;
@@ -86,27 +84,27 @@ int main() {
 
 	// Process CasosAcumuDistrito2020P1.csv
 	// Process CasosAcumuDistrito2020P2.csv
-	//{
-	//	tl::Table* input_raw_table_p1;
-	//	tl::Table* input_raw_table_p2;
-	//	process_ca_distr_20(input_raw_table_p1, input_raw_table_p2, main_config, distr_index);
-	//	append_end_ca_distr_20(input_raw_table_p1, main_config, report_date, distr_index);
-	//	//append_begin_ca_distr_20(input_raw_table_p1, main_config, areas_config, distr_index);
-	//	delete input_raw_table_p1;
-	//	delete input_raw_table_p2;
-	//}
+	{
+		tl::Table* input_raw_table_p1;
+		tl::Table* input_raw_table_p2;
+		process_ca_distr_20(input_raw_table_p1, input_raw_table_p2, main_config, distr_index);
+		append_end_ca_distr_20(input_raw_table_p1, main_config, report_date, distr_index);
+		//append_begin_ca_distr_20(input_raw_table_p1, main_config, areas_config, distr_index);
+		delete input_raw_table_p1;
+		delete input_raw_table_p2;
+	}
 	
 	// Process CasosAcumuDistrito2021P1.csv
 	// Process CasosAcumuDistrito2021P2.csv
-	//{
-	//	tl::Table* input_raw_table_p1;
-	//	tl::Table* input_raw_table_p2;
-	//	process_ca_distr_21(input_raw_table_p1, input_raw_table_p2, main_config, distr_index);
-	//	append_end_ca_distr_21(input_raw_table_p1, main_config, report_date, distr_index);
-	//	//append_begin_ca_distr_21(input_raw_table_p1, main_config, areas_config, distr_index);
-	//	delete input_raw_table_p1;
-	//	delete input_raw_table_p2;
-	//}
+	{
+		tl::Table* input_raw_table_p1;
+		tl::Table* input_raw_table_p2;
+		process_ca_distr_21(input_raw_table_p1, input_raw_table_p2, main_config, distr_index);
+		append_end_ca_distr_21(input_raw_table_p1, main_config, report_date, distr_index);
+		//append_begin_ca_distr_21(input_raw_table_p1, main_config, areas_config, distr_index);
+		delete input_raw_table_p1;
+		delete input_raw_table_p2;
+	}
 	
 	// Process MuertesAcumulaDistritoP1.csv
 	// Process MuertesAcumulaDistritoP2.csv
